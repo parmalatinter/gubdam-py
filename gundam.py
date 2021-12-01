74c640c338e491e0e7ee90b8865e5e2f294815d9
Yosuke Kobayashi <parmalatinter@gmail.com>
2021-12-01 22:45:13 +0900

    Update gundam.py

--- gundam.py
+++ gundam.py
@@ -13,20 +13,27 @@ from libs import music
 # sphinx autoapi required
 __all__ = ["LedButton", 'GroveLed', 'GPIO', 'Music']
 
+
 class LedButton(object):
 
     def __init__(self, pin = 16):
         # High = light on
-        self.led = Factory.getOneLed("GPIO-HIGH", pin)
+        self.buttonLed = Factory.getOneLed("GPIO-HIGH", pin)
+        self.buttonLed.light(True)
+        self.headLed = Factory.getOneLed("GPIO-HIGH", 8)
+        self.headLed.light(True)
+        self.led = Factory.getOneLed("GPIO-HIGH", 5)
         self.led.light(True)
-        self.led2 = led.Led(5)
+
+        self.oled = oled.Oled()
+
         self.music = music.Music()
+
         # Low = pressed
         self.__btn = Factory.getButton("GPIO-LOW", pin + 1)
         self.__on_event = None
         self.__btn.on_event(self, LedButton.__handle_event)
-        self.oled = oled.Oled()
-        self.headLed = led.Led(8)
+
         self.oled.show('This is The Gundam')
 
 
@@ -41,35 +48,46 @@ class LedButton(object):
         self.__on_event = callback
 
     def __handle_event(self, evt):
-        # print("event index:{} event:{} pressed:{}"
-        #       .format(evt['index'], evt['code'], evt['presesed']))
+        print("event index:{} event:{}".format(evt['index'], evt['code']))
+
         if callable(self.__on_event):
             # the customized behavior
             self.__on_event(evt['index'], evt['code'], evt['time'])
             return
 
-        # the default behavior
+        self.buttonLed.brightness = self.buttonLed.MAX_BRIGHT
         self.led.brightness = self.led.MAX_BRIGHT
+        self.headLed.brightness = self.headLed.MAX_BRIGHT
 
         event = evt['code']
-        if event & Button.EV_SINGLE_CLICK:
+
+        if event & Button.EV_DOUBLE_CLICK:
+            self.buttonLed.light(True)
             self.led.light(True)
-            self.led2.on()
+            self.headLed.light(True)
+
+            print("EV_DOUBLE_CLICK")
+
+        elif event & Button.EV_SINGLE_CLICK:
+            self.buttonLed.blink()
+            self.led.blink()
+            self.headLed.blink()
             self.music.on()
             self.oled.show('Power On Gundam')
-            self.headLed.blight()
 
-        elif event & Button.EV_DOUBLE_CLICK:
-            self.led.blink()
-            print("blink    LED")
+            print("EV_SINGLE_CLICK")
+
         elif event & Button.EV_LONG_PRESS:
+            self.buttonLed.light(False)
             self.led.light(False)
-            self.led2.off()
+            self.headLed.light(False)
             self.music.off()
             self.oled.show('Power Off Gundam')
-            self.headLed.off()
-        elif event & Button.EV_LEVEL_CHANGED:
-            self.oled.show('EV LEVEL CHANGED')
+
+            print("EV_LONG_PRESS")
+        # elif event & Button.EV_LEVEL_CHANGED:
+        #     print('EV LEVEL CHANGED')
+            # self.oled.show('EV LEVEL CHANGED')
 
 
 def main():
@@ -77,14 +95,6 @@ def main():
 
     ledbtn = LedButton(pin)
 
-    # remove ''' pairs below to begin your experiment
-    '''
-    # define a customized event handle your self
-    def cust_on_event(index, event, tm):
-        print("event with code {}, time {}".format(event, tm))
-
-    ledbtn.on_event = cust_on_event
-    '''
     while True:
         time.sleep(1)
 
