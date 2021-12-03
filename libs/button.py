d9a8983ac06814033fa80dc89af431f2b863ba90
Yosuke Kobayashi <parmalatinter@gmail.com>
2021-12-04 00:40:09 +0900

    Add files via upload

---
+++ libs/button.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import time
+from grove.button import Button
+from grove.factory import Factory
+
+__all__ = ['NormalButton']
+
+class NormalButton(object):
+
+    def __init__(self, pin):
+        # High = pressed
+        self.__btn = Factory.getButton("GPIO-HIGH", pin)
+        self.__last_time = time.time()
+        self.__on_press = None
+        self.__on_release = None
+        self.__btn.on_event(self, NormalButton.__handle_event)
+
+    @property
+    def on_press(self):
+        return self.__on_press
+
+    @on_press.setter
+    def on_press(self, callback):
+        if not callable(callback):
+            return
+        self.__on_press = callback
+
+    @property
+    def on_release(self):
+        return self.__on_release
+
+    @on_release.setter
+    def on_release(self, callback):
+        if not callable(callback):
+            return
+        self.__on_release = callback
+
+    def __handle_event(self, evt):
+        dt, self.__last_time = evt["time"] - self.__last_time, evt["time"]
+        if evt["code"] == Button.EV_LEVEL_CHANGED:
+            if evt["pressed"]:
+                if callable(self.__on_press):
+                    self.__on_press(dt)
+            else:
+                if callable(self.__on_release):
+                    self.__on_release(dt)
+
---
+++ libs/const.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+class _const:
+    class ConstError(TypeError):
+        pass
+
+    def __setattr__(self, name, value):
+        if name in self.__dict__:
+            raise self.ConstError("Can't rebind const (%s)" % name)
+        self.__dict__[name] = value
+
+import sys
+sys.modules[__name__]=_const()
